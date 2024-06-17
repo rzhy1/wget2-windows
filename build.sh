6faf3b7fdf0cd4866fc71e33b5f36735dcf8f51e
rzhy1 <120646685+rzhy1@users.noreply.github.com>
2024-06-17 20:14:01 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -39,8 +39,8 @@ cd brotli
 CMAKE_SYSTEM_NAME=Windows CMAKE_C_COMPILER=x86_64-w64-mingw32-gcc CMAKE_CXX_COMPILER=x86_64-w64-mingw32-g++ cmake . -DCMAKE_INSTALL_PREFIX=$INSTALLDIR -DBUILD_SHARED_LIBS=OFF
 make install
 cd .. && rm -rf brotli
-apt-cache show libbrotlienc | grep Version
-apt-cache show libbrotlidec | grep Version
+apt-cache show brotli | grep Version
+apt-cache show libbrotli | grep Version
 
 echo "⭐⭐⭐⭐⭐⭐$(date '+%Y/%m/%d %a %H:%M:%S.%N') - build libiconv⭐⭐⭐⭐⭐⭐" 
 wget -O- https://ftp.gnu.org/gnu/libiconv/libiconv-1.17.tar.gz | tar xz
