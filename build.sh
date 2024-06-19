d8ff288e9482fc0042ff1b03955868458a368d1e
rzhy1 <120646685+rzhy1@users.noreply.github.com>
2024-06-19 14:53:33 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,7 +3,6 @@
 # Author: rzhy1
 # 2024/6/16
 
-
 # 设置环境变量
 export PREFIX="x86_64-w64-mingw32"
 export INSTALLDIR="$HOME/usr/local/$PREFIX"
@@ -24,8 +23,6 @@ export WINEPATH="$INSTALLDIR/bin;$INSTALLDIR/lib;/usr/$PREFIX/bin;/usr/$PREFIX/l
 # export BZ2_LIBS="-L/usr/lib/x86_64-linux-gnu -lbz2"
 # export BROTLIDEC_CFLAGS="-I$INSTALLDIR/include"
 # export BROTLIDEC_LIBS="-L$INSTALLDIR/lib -lbrotlidec"
-  export BROTLI_CFLAGS="-I/home/runner/usr/local/x86_64-w64-mingw32/include"
-  export BROTLI_LIBS="-L/home/runner/usr/local/x86_64-w64-mingw32/lib -lbrotlidec"
 mkdir -p $INSTALLDIR
 cd $INSTALLDIR
 
@@ -136,7 +133,7 @@ echo "⭐⭐⭐⭐⭐⭐$(date '+%Y/%m/%d %a %H:%M:%S.%N') - build wget2⭐⭐�
 git clone https://github.com/rockdaboot/wget2.git
 cd wget2
 ./bootstrap --skip-po
-LDFLAGS="-Wl,-Bstatic,--whole-archive -lwinpthread -Wl,--no-whole-archive" CFLAGS="-O2 -DNGHTTP2_STATICLIB" ./configure $CONFIGURE_BASE_FLAGS --build=x86_64-pc-linux-gnu --host=$PREFIX --disable-shared --enable-static --with-lzma --with-zstd --with-bzip2 --with-lzip --with-brotlidec --without-gpgme  --enable-threads=windows 
+LDFLAGS="-Wl,-Bstatic,--whole-archive -lwinpthread -Wl,--no-whole-archive" CFLAGS="-O2 -DNGHTTP2_STATICLIB" ./configure $CONFIGURE_BASE_FLAGS --build=x86_64-pc-linux-gnu --host=$PREFIX --disable-shared --enable-static --with-lzma --with-zstd --with-bzip2 --with-lzip --with-brotlidec `pkg-config --cflags --libs libbrotlidec` --without-gpgme  --enable-threads=windows 
 make -j$(nproc)
 strip $INSTALLDIR/wget2/src/wget2.exe
 cp -fv "$INSTALLDIR/wget2/src/wget2.exe" "${GITHUB_WORKSPACE}"
