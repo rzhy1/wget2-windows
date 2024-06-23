aa0ae0c1c3c4220b95c7bdc15cd68a8f62e58621
rzhy1 <120646685+rzhy1@users.noreply.github.com>
2024-06-23 10:00:34 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -106,7 +106,7 @@ echo "⭐⭐⭐⭐⭐⭐$(date '+%Y/%m/%d %a %H:%M:%S.%N') - build gpgme⭐⭐�
 wget -O- https://gnupg.org/ftp/gcrypt/gpgme/gpgme-1.23.2.tar.bz2 | tar xj
 cd gpgme-* || exit
 find / -name "python3*" 2>/dev/null
-env PYTHON=$pwd/myenv/bin/python3.12 ./configure --host=$PREFIX --disable-shared --prefix="$INSTALLDIR" --enable-static --with-libgpg-error-prefix="$INSTALLDIR" --disable-gpg-test --disable-g13-test --disable-gpgsm-test --disable-gpgconf-test --disable-glibtest --with-libassuan-prefix="$INSTALLDIR" || exit 1
+env PYTHON=$PWD/myenv/bin/python3.12 ./configure --host=$PREFIX --disable-shared --prefix="$INSTALLDIR" --enable-static --with-libgpg-error-prefix="$INSTALLDIR" --disable-gpg-test --disable-g13-test --disable-gpgsm-test --disable-gpgconf-test --disable-glibtest --with-libassuan-prefix="$INSTALLDIR" || exit 1
 make -j$(nproc) || exit 1
 make install || exit 1
 cd .. && rm -rf gpgme-*
