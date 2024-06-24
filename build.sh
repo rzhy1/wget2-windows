22eeead4bf01e259944f3f9188ef2d44cca7c378
rzhy1 <120646685+rzhy1@users.noreply.github.com>
2024-06-24 21:44:13 +0800

    Update vcpkg

--- build.sh
+++ build.sh
@@ -97,6 +97,7 @@ cd vcpkg
 ./bootstrap-vcpkg.sh
 ./vcpkg integrate install
 ./vcpkg install brotli
+cd .. && rm -rf vcpkg
 #git clone --depth 1 https://github.com/google/brotli.git || exit 1
 #cd brotli || exit 1
 #CMAKE_SYSTEM_NAME=Windows CMAKE_C_COMPILER=x86_64-w64-mingw32-gcc CMAKE_CXX_COMPILER=x86_64-w64-mingw32-g++ cmake . -DCMAKE_INSTALL_PREFIX=$INSTALLDIR -DBUILD_SHARED_LIBS=OFF -DCMAKE_BUILD_TYPE=Release || exit 1
