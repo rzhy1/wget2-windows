d4b5b7b40d1baef65de3897915429c0deb7169de
rzhy1 <120646685+rzhy1@users.noreply.github.com>
2024-06-27 23:01:21 +0800

    新的gnutls-3.8.5

--- build.sh
+++ build.sh
@@ -148,9 +148,25 @@ make install || exit 1
 cd .. && rm -rf nettle
 
 echo "⭐⭐⭐⭐⭐⭐$(date '+%Y/%m/%d %a %H:%M:%S.%N') - build gnutls⭐⭐⭐⭐⭐⭐" 
-wget -O- https://www.gnupg.org/ftp/gcrypt/gnutls/v3.8/gnutls-3.8.3.tar.xz | tar x --xz || exit 1
+#wget -O- https://www.gnupg.org/ftp/gcrypt/gnutls/v3.8/gnutls-3.8.3.tar.xz | tar x --xz || exit 1
+#cd gnutls-* || exit 1
+#./configure --build=x86_64-pc-linux-gnu --host=$PREFIX --prefix=$INSTALLDIR --with-nettle-mini --disable-shared --enable-static --with-included-libtasn1 --with-included-unistring --without-p11-kit --disable-doc --disable-tests --disable-full-test-suite --disable-tools --disable-cxx --disable-maintainer-mode --disable-libdane --disable-hardware-acceleration --disable-guile || exit 1
+#make -j$(nproc) || exit 1
+#make install || exit 1
+#cd .. && rm -rf gnutls-*
+wget -O- https://www.gnupg.org/ftp/gcrypt/gnutls/v3.8/gnutls-3.8.5.tar.xz | tar x --xz || exit 1
 cd gnutls-* || exit 1
-./configure --build=x86_64-pc-linux-gnu --host=$PREFIX --prefix=$INSTALLDIR --with-nettle-mini --disable-shared --enable-static --with-included-libtasn1 --with-included-unistring --without-p11-kit --disable-doc --disable-tests --disable-full-test-suite --disable-tools --disable-cxx --disable-maintainer-mode --disable-libdane --disable-hardware-acceleration --disable-guile || exit 1
+  GMP_LIBS="-L$INSTALLDIR/lib -lgmp" \
+  NETTLE_LIBS="-L$INSTALLDIR/lib -lnettle -lgmp" \
+  HOGWEED_LIBS="-L$INSTALLDIR/lib -lhogweed -lnettle -lgmp" \
+  LIBTASN1_LIBS="-L$INSTALLDIR/lib -ltasn1" \
+  LIBIDN2_LIBS="-L$INSTALLDIR/lib -lidn2" \
+  GMP_CFLAGS=$CFLAGS \
+  LIBTASN1_CFLAGS=$CFLAGS \
+  NETTLE_CFLAGS=$CFLAGS \
+  HOGWEED_CFLAGS=$CFLAGS \
+  LIBIDN2_CFLAGS=$CFLAGS \
+./configure --host=$PREFIX --prefix="$INSTALLDIR" --with-included-unistring --disable-openssl-compatibility --without-p11-kit --disable-tests --disable-doc --disable-shared --enable-static
 make -j$(nproc) || exit 1
 make install || exit 1
 cd .. && rm -rf gnutls-*
