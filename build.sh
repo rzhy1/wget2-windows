ffa6c67f18b164ecba69652a16bc402768ee31e0
rzhy1 <120646685+rzhy1@users.noreply.github.com>
2024-06-29 21:30:16 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -84,12 +84,12 @@ meson install -C builddir-st || exit 1
 cd .. && rm -rf zstd
 
 echo "⭐⭐⭐⭐⭐⭐$(date '+%Y/%m/%d %a %H:%M:%S.%N') - build zlib⭐⭐⭐⭐⭐⭐" 
-#wget -O- https://zlib.net/zlib-1.3.1.tar.gz | tar xz || exit 1
-#cd zlib-* || exit 1
-#CC=x86_64-w64-mingw32-gcc ./configure --64 --static --prefix="$INSTALLDIR"
-#make -j$(nproc) || exit 1
-#make install || exit 1
-#cd .. && rm -rf zlib
+wget -O- https://zlib.net/zlib-1.3.1.tar.gz | tar xz || exit 1
+cd zlib-* || exit 1
+CC=x86_64-w64-mingw32-gcc ./configure --64 --static --prefix="$INSTALLDIR"
+make -j$(nproc) || exit 1
+make install || exit 1
+cd .. && rm -rf zlib
 
 echo "⭐⭐⭐⭐⭐⭐$(date '+%Y/%m/%d %a %H:%M:%S.%N') - build gmp⭐⭐⭐⭐⭐⭐" 
 wget -nv -O- https://ftp.gnu.org/gnu/gmp/gmp-6.3.0.tar.xz | tar x --xz || exit 1
@@ -161,14 +161,14 @@ make -j$(nproc) || exit 1
 make install || exit 1
 cd .. && rm -rf libtasn1-*
 
-export LDFLAGS="-L$INSTALLDIR/lib -Wl,--as-needed $LDFLAGS -lbrotlienc -lbrotlidec -lbrotlicommon"
+#export LDFLAGS="-L$INSTALLDIR/lib -Wl,--as-needed $LDFLAGS -lbrotlienc -lbrotlidec -lbrotlicommon"
 echo "⭐⭐⭐⭐⭐⭐$(date '+%Y/%m/%d %a %H:%M:%S.%N') - build gnutls⭐⭐⭐⭐⭐⭐" 
-wget -O- https://www.gnupg.org/ftp/gcrypt/gnutls/v3.8/gnutls-3.8.3.tar.xz | tar x --xz || exit 1
-cd gnutls-* || exit 1
-./configure --build=x86_64-pc-linux-gnu --host=$PREFIX --prefix=$INSTALLDIR --with-nettle-mini --disable-shared --enable-static --with-included-libtasn1 --with-included-unistring --without-p11-kit --disable-doc --disable-tests --disable-full-test-suite --disable-tools --disable-cxx --disable-maintainer-mode --disable-libdane --disable-hardware-acceleration --disable-guile || exit 1
-make -j$(nproc) || exit 1
-make install || exit 1
-cd .. && rm -rf gnutls-*
+#wget -O- https://www.gnupg.org/ftp/gcrypt/gnutls/v3.8/gnutls-3.8.3.tar.xz | tar x --xz || exit 1
+#cd gnutls-* || exit 1
+#./configure --build=x86_64-pc-linux-gnu --host=$PREFIX --prefix=$INSTALLDIR --with-nettle-mini --disable-shared --enable-static --with-included-libtasn1 --with-included-unistring --without-p11-kit --disable-doc --disable-tests --disable-full-test-suite --disable-tools --disable-cxx --disable-maintainer-mode --disable-libdane --disable-hardware-acceleration --disable-guile || exit 1
+#make -j$(nproc) || exit 1
+#make install || exit 1
+#cd .. && rm -rf gnutls-*
 
 #wget -O- https://www.gnupg.org/ftp/gcrypt/gnutls/v3.8/gnutls-3.8.3.tar.xz | tar x --xz || exit 1
 #cd gnutls-* || exit 1
@@ -219,13 +219,20 @@ make -j$(nproc)  || exit 1
 make install || exit 1
 cd .. && rm -rf libmicrohttpd-*
 
+echo "⭐⭐⭐⭐⭐⭐$(date '+%Y/%m/%d %a %H:%M:%S.%N') - build openssl⭐⭐⭐⭐⭐⭐" 
+wget -O- https://www.openssl.org/source/openssl-1.1.1w.tar.gz | tar xz || exit 1
+cd openssl-* || exit 1
+  ./Configure --static -static --prefix="$INSTALLDIR" --cross-compile-prefix=x86_64-w64-mingw32- mingw64 no-shared enable-asm no-tests --with-zlib-include="$INSTALLDIR" --with-zlib-lib="$INSTALLDIR"/lib/libz.a || exit 1
+ make -j$(nproc) || exit 1
+ make install_sw || exit 1
+
 echo "⭐⭐⭐⭐⭐⭐$(date '+%Y/%m/%d %a %H:%M:%S.%N') - build wget2⭐⭐⭐⭐⭐⭐" 
 git clone https://github.com/rockdaboot/wget2.git || exit 1
 cd wget2 || exit 1
 ./bootstrap --skip-po || exit 1
 export LDFLAGS="-Wl,-Bstatic,--whole-archive -lwinpthread -Wl,--no-whole-archive"
 export CFLAGS="-O2 -DNGHTTP2_STATICLIB"
-./configure --build=x86_64-pc-linux-gnu --host=$PREFIX --with-libiconv-prefix="$INSTALLDIR" --disable-shared --enable-static --with-lzma --with-zstd --without-bzip2 --without-lzip --without-brotlidec --without-gpgme --enable-threads=windows || exit 1
+./configure --build=x86_64-pc-linux-gnu --host=$PREFIX --with-libiconv-prefix="$INSTALLDIR" --with-ss=openssl --disable-shared --enable-static --with-lzma --with-zstd --without-bzip2 --without-lzip --without-brotlidec --without-gpgme --enable-threads=windows || exit 1
 make -j$(nproc) || exit 1
 strip $INSTALLDIR/wget2/src/wget2.exe || exit 1
 cp -fv "$INSTALLDIR/wget2/src/wget2.exe" "${GITHUB_WORKSPACE}" || exit 1
