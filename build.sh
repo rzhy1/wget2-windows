7c72bb64ec6f02f6b2838915108c1f6751192a30
rzhy1 <120646685+rzhy1@users.noreply.github.com>
2024-06-30 17:06:33 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -220,9 +220,9 @@ git clone https://github.com/rockdaboot/wget2.git || exit 1
 cd wget2 || exit 1
 ./bootstrap --skip-po || exit 1
 ./configure \
-  OPENSSL_CFLAGS="-I/home/runner/usr/local/x86_64-w64-mingw32/openssl-3.2.1/include" \
-  OPENSSL_LIBS="-L/home/runner/usr/local/x86_64-w64-mingw32/openssl-3.2.1/lib64 -lcrypto -lssl -lbcrypt -lcrypt32" \
-  LDFLAGS="-Wl,-Bstatic,--whole-archive -lwinpthread -Wl,--no-whole-archive -Wl,-rpath,/home/runner/usr/local/x86_64-w64-mingw32/lib64 -L/usr/x86_64-w64-mingw32/lib -Wl,--verbose" \
+  OPENSSL_CFLAGS="-I/$INSTALLDIR/include" \
+  OPENSSL_LIBS="-L$INSTALLDIR/lib64 -lcrypto -lssl -lbcrypt -lcrypt32" \
+  LDFLAGS="-Wl,-Bstatic,--whole-archive -lwinpthread -Wl,--no-whole-archive -Wl,-rpath,$INSTALLDIR/lib64 -L/usr/x86_64-w64-mingw32/lib -Wl,--verbose" \
   CFLAGS="-O2 -DNGHTTP2_STATICLIB" \
   --build=x86_64-pc-linux-gnu \
   --host=$PREFIX \
