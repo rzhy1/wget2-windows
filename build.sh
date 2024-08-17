214c2be9e59ec299e678efcbc72ac9db2d0d2edf
rzhy1 <120646685+rzhy1@users.noreply.github.com>
2024-08-17 09:23:43 +0800

    git clone https://gitlab.com/gnutls/gnutls.git

--- build.sh
+++ build.sh
@@ -159,8 +159,11 @@ make install || exit 1
 cd .. && rm -rf libtasn1-*
 
 echo "⭐⭐⭐⭐⭐⭐$(date '+%Y/%m/%d %a %H:%M:%S.%N') - build gnutls⭐⭐⭐⭐⭐⭐" 
-wget -O- https://www.gnupg.org/ftp/gcrypt/gnutls/v3.8/gnutls-3.8.7.tar.xz | tar x --xz || exit 1
-cd gnutls-* || exit 1
+#wget -O- https://www.gnupg.org/ftp/gcrypt/gnutls/v3.8/gnutls-3.8.7.tar.xz | tar x --xz || exit 1
+#cd gnutls-* || exit 1
+git clone https://gitlab.com/gnutls/gnutls.git || exit 1
+cd gnutls || exit 1
+./bootstrap || exit 1
 GMP_LIBS="-L$INSTALLDIR/lib -lgmp" \
 NETTLE_LIBS="-L$INSTALLDIR/lib -lnettle -lgmp" \
 HOGWEED_LIBS="-L$INSTALLDIR/lib -lhogweed -lnettle -lgmp" \
