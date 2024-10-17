cd0fe510efc6c79510c9ad13b5666cf36aeb4aaa
rzhy1 <120646685+rzhy1@users.noreply.github.com>
2024-10-17 18:52:04 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -230,7 +230,7 @@ build_gnutls() {
   NETTLE_CFLAGS=$CFLAGS \
   HOGWEED_CFLAGS=$CFLAGS \
   LIBIDN2_CFLAGS=$CFLAGS \
-  ./configure CFLAGS="-O3" --host=$PREFIX --prefix=$INSTALLDIR --disable-openssl-compatibility --disable-hardware-acceleration --disable-shared --enable-static --without-p11-kit --disable-doc --disable-tests --disable-full-test-suite --disable-tools --disable-cxx --disable-maintainer-mode --disable-libdane || exit 1
+  ./configure CFLAGS="-O3" --host=$PREFIX --prefix=$INSTALLDIR --with-included-unistring --disable-openssl-compatibility --disable-hardware-acceleration --disable-shared --enable-static --without-p11-kit --disable-doc --disable-tests --disable-full-test-suite --disable-tools --disable-cxx --disable-maintainer-mode --disable-libdane || exit 1
   # --build=x86_64-pc-linux-gnu --with-nettle-mini --with-included-libtasn1 
   make -j$(nproc) || exit 1
   make install || exit 1
