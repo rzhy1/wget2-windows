d223eee9748b1099ed5b86dd3e3da521cacaa391
rzhy1 <120646685+rzhy1@users.noreply.github.com>
2024-10-17 23:05:42 +0800

    #build_dlfcn-win32 &

--- build.sh
+++ build.sh
@@ -258,24 +258,18 @@ build_wget2() {
 
 build_xz &
 build_zstd &
-wait
 build_zlib-ng &
 build_gmp &
-wait
 #build_gnulibmirror &
 build_libiconv &
 build_libunistring &
-wait
 build_libidn2 &
 build_libtasn1 &
-wait
 build_PCRE2 &
 build_nghttp2 &
-wait
-build_dlfcn-win32 &
+#build_dlfcn-win32 &
 build_libmicrohttpd &
 wait
-
 build_libpsl
 build_nettle
 build_gnutls
