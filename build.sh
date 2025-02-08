c201ecbec8e17d97f921da873e65cd394c00a4a3
rzhy1 <120646685+rzhy1@users.noreply.github.com>
2025-02-08 22:17:17 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -316,7 +316,7 @@ build_wget2() {
   #LIBPCRE2_LIBS="-L$INSTALLDIR/lib -lpcre2-8"  \
   #BROTLIDEC_CFLAGS="-I$INSTALLDIR/include" \
   #BROTLIDEC_LIBS="-L$INSTALLDIR/libb -lbrotlienc -lbrotlidec -lbrotlicommon" \
-  ./configure --build=x86_64-pc-linux-gnu --host=$PREFIX --with-libiconv-prefix="$INSTALLDIR" --with-ssl=gnutls --disable-shared --enable-static --without-lzma  --with-zstd --with-brotlidec  --without-bzip2 --without-lzip --without-gpgme --enable-threads=windows || exit 1
+  ./configure --build=x86_64-pc-linux-gnu --host=$PREFIX --with-libiconv-prefix="$INSTALLDIR" --with-ssl=gnutls --disable-shared --enable-static --without-lzma  --with-zstd --without-brotlidec  --without-bzip2 --without-lzip --without-gpgme --enable-threads=windows || exit 1
   make -j$(nproc) || exit 1
   strip $INSTALLDIR/wget2/src/wget2.exe || exit 1
   cp -fv "$INSTALLDIR/wget2/src/wget2.exe" "${GITHUB_WORKSPACE}" || exit 1
