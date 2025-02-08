c51fd144584bd3d064466cf0c4fba6ddc80081da
rzhy1 <120646685+rzhy1@users.noreply.github.com>
2025-02-09 00:49:19 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -135,6 +135,7 @@ build_brotli() {
   pkg-config --libs libbrotlidec
   pkg-config --cflags --libs libbrotlidec
   pkg-config --cflags --libs libbrotlienc libbrotlidec libbrotlicommon
+  pkg-config --variable pc_path pkg-config
   echo "查询"
   /usr/bin/x86_64-w64-mingw32-ld --verbose | grep brotli
   echo "查询1"
