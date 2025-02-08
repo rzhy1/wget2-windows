ed1fb8326dc641d07c292d65d1b9676f5119e7fa
rzhy1 <120646685+rzhy1@users.noreply.github.com>
2025-02-08 19:57:28 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -26,14 +26,14 @@ cd $INSTALLDIR
 build_xz() {
   echo "⭐⭐⭐⭐⭐⭐$(date '+%Y/%m/%d %a %H:%M:%S.%N') - build xz⭐⭐⭐⭐⭐⭐" 
   local start_time=$(date +%s.%N)
-  sudo apt-get purge xz-utils
+  apt-get purge xz-utils
   git clone -j$(nproc) https://github.com/tukaani-project/xz.git || { echo "Git clone failed"; exit 1; }
   cd xz || { echo "cd xz failed"; exit 1; }
   mkdir build
   cd build
-  sudo cmake .. -DCMAKE_INSTALL_PREFIX=/usr/local -DCMAKE_BUILD_TYPE=Release -DXZ_NLS=ON -DBUILD_SHARED_LIBS=OFF || { echo "CMake failed"; exit 1; }
-  sudo cmake --build . -- -j$(nproc) || { echo "Build failed"; exit 1; }
-  sudo cmake --install . || { echo "Install failed"; exit 1; }
+  cmake .. -DCMAKE_INSTALL_PREFIX=/usr/local -DCMAKE_BUILD_TYPE=Release -DXZ_NLS=ON -DBUILD_SHARED_LIBS=OFF || { echo "CMake failed"; exit 1; }
+  cmake --build . -- -j$(nproc) || { echo "Build failed"; exit 1; }
+  cmake --install . || { echo "Install failed"; exit 1; }
   xz --version
   cd ../.. && rm -rf xz
   local end_time=$(date +%s.%N)
@@ -66,11 +66,11 @@ build_zstd() {
     -Ddefault_library=static \
     -Db_lto=true --optimization=2 \
     build/meson builddir-st || exit 1
-  sudo rm -f /usr/local/bin/zstd*
-  sudo rm -f /usr/local/bin/*zstd
+  rm -f /usr/local/bin/zstd*
+  rm -f /usr/local/bin/*zstd
   meson compile -C builddir-st || exit 1
   meson install -C builddir-st || exit 1
-  find / type -f -name "*zstd*" 2>/dev/null
+  find / -type f -name "*zstd*" 2>/dev/null
   zstd --version
   cd .. && rm -rf zstd
   local end_time=$(date +%s.%N)
