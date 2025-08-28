b6d748c1ea8fe96d8727198b0845b8801871df56
rzhy1 <120646685+rzhy1@users.noreply.github.com>
2025-08-28 19:29:42 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -306,13 +306,16 @@ build_wget2() {
   git submodule init
   git config submodule.gnulib.url https://github.com/coreutils/gnulib.git
   git submodule update --depth=1 --init gnulib
+  cd gnulib || exit 1
+  git pull origin master
+  cd .. || exit 1
   ./bootstrap --skip-po || exit 1
-  export CPPFLAGS="-I$INSTALLDIR/include -DGNULIB_NAMESPACE=wget_gnulib"
   export LDFLAGS="$LDFLAGS -L$INSTALLDIR/lib -Wl,-Bstatic,--whole-archive -lwinpthread -Wl,--no-whole-archive"
-  export CFLAGS="-L$INSTALLDIR/include -DNGHTTP2_STATICLIB -DGNULIB_NAMESPACE=wget_gnulib $CFLAGS"
+  export CFLAGS="-L$INSTALLDIR/include -DNGHTTP2_STATICLIB $CFLAGS"
   export LIBS="-lgnutls -lpsl -lpcre2-8 -lidn2 -lzstd -lbrotlienc -lbrotlidec -lbrotlicommon -lz -lnghttp2 -lbcrypt -lncrypt"
   export ac_cv_func_fcntl=no
   export gl_cv_header_error_h=no
+  ac_cv_func_fcntl=no gl_cv_header_error_h=no \
   GNUTLS_CFLAGS=$CFLAGS \
   GNUTLS_LIBS="-L$INSTALLDIR/lib -lgnutls -lbcrypt -lncrypt" \
   LIBPSL_CFLAGS=$CFLAGS \
