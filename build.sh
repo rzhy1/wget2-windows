839a013ec0737161cde2dd52ac83a441b537ecd8
rzhy1 <120646685+rzhy1@users.noreply.github.com>
2025-08-28 18:17:35 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -309,10 +309,11 @@ build_wget2() {
   ./bootstrap --skip-po || exit 1
   export LDFLAGS="$LDFLAGS -L$INSTALLDIR/lib -Wl,-Bstatic,--whole-archive -lwinpthread -Wl,--no-whole-archive"
   export CFLAGS="-L$INSTALLDIR/include -DNGHTTP2_STATICLIB $CFLAGS"
-  export LIBS="-lbrotlienc -lbrotlidec -lbrotlicommon"
-  #export LIBS="-lgnutls -lpsl -lpcre2-8 -lidn2 -lzstd -lbrotlienc -lbrotlidec -lbrotlicommon -lz -lnghttp2 -lbcrypt -lncrypt"
-  #export ac_cv_func_fcntl=no
+  #export LIBS="-lbrotlienc -lbrotlidec -lbrotlicommon"
+  export LIBS="-lgnutls -lpsl -lpcre2-8 -lidn2 -lzstd -lbrotlienc -lbrotlidec -lbrotlicommon -lz -lnghttp2 -lbcrypt -lncrypt"
+  export ac_cv_func_fcntl=no
   #export ac_cv_func_error_at_line=no
+  export gl_cv_header_error_h=no
   GNUTLS_CFLAGS=$CFLAGS \
   GNUTLS_LIBS="-L$INSTALLDIR/lib -lgnutls -lbcrypt -lncrypt" \
   LIBPSL_CFLAGS=$CFLAGS \
